4ea73ad00654599d5322d11753275209c0ffa238
himscode <himalay@quaditworld.com>
2021-04-28 13:31:50 +0530

    ProcFile

--- app/VPNServer.php
+++ app/VPNServer.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class VPNServer extends Model
 {
-    
     protected $table = "vpnserver";
     protected $hidden = ["config","username","password","status"];
     protected $fillable = ["name","config","flag", "slug","username","password","status"];
