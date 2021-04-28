5cf694c2209f490595c9e146677884d5cea68c99
himscode <himalay@quaditworld.com>
2021-04-28 13:19:30 +0530

    Initial import

---
+++ app/VPNServer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VPNServer extends Model
+{
+    
+    protected $table = "vpnserver";
+    protected $hidden = ["config","username","password","status"];
+    protected $fillable = ["name","config","flag", "slug","username","password","status"];
+}
